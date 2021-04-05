71a80dcce44d65c5b83d4255e3f3210e3db8b8d8
ryusei <perusonaryu1127@gmail.com>
2021-04-05 20:33:53 +0900

    猫詳細画面修正　猫編集ページ削除ボタン修正

--- resources/js/components/myPageComponent/myCatsList.vue
+++ resources/js/components/myPageComponent/myCatsList.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-row justify="center">
+  <v-row justify="space-between">
     <v-col v-for="catData in myCats" :key="catData.name" md="6">
       <v-card class="mx-auto" max-width="400" outlined>
         <v-list-item three-line>
@@ -27,9 +27,25 @@
             </v-btn>
           </router-link>
           <v-spacer></v-spacer>
-          <v-btn outlined rounded text color="red" @click="catDelete(catData.id)">
+          <v-dialog transition="dialog-top-transition" max-width="600" class="delete-dialog">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="red" outlined rounded text v-bind="attrs" v-on="on">削除</v-btn>
+            </template>
+            <template v-slot:default="dialog">
+              <v-card>
+                <v-toolbar color="red" dark class="text-h5 mb-5">本当に削除しますか？</v-toolbar>
+                <v-card-text>
+                  <div class="d-flex justify-center">
+                    <v-btn color="red" outlined rounded text @click="catDelete(catData.id)" >はい</v-btn>
+                    <v-btn outlined rounded text @click="dialog.value = false">いいえ</v-btn>
+                  </div>
+                </v-card-text>
+              </v-card>
+            </template>
+          </v-dialog>
+          <!-- <v-btn outlined rounded text color="red" @click="catDelete(catData.id)">
             削除
-          </v-btn>
+          </v-btn> -->
         </v-card-actions>
       </v-card>
     </v-col>
@@ -72,10 +88,17 @@ export default {
         })
         .then(result => {
           this.$store.dispatch('cat/myCatsListGet', { userId: this.user.id, token: token });
+          dialog.value = false
         });
+
     },
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+
+.delete-dialog{
+  height: 400px;
+}
+</style>
